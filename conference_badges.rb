9eaa11c722c3d1e431a31915041cdaaa821446af
Corina Talavera <corina.talavera@gmail.com>
2018-10-27 01:32:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,5 +10,6 @@ def batch_badge_creator(attendees)
 def assign_rooms(attendees)
   room = 1 
   attendees.collect do |name|
-  puts "Hello, my name is #{name}!You'll be assigned to "
+  puts "Hello, my name is #{name}!You'll be assigned to room #{room}"
+  
 end
