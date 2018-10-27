a13cf0389e2d2f5fbdf402267687b70452656e8d
Corina Talavera <corina.talavera@gmail.com>
2018-10-27 01:29:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees = []
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   attendees.collect do |name|
   end 
 end
